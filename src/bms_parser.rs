450cf69d1aa9bbdcd8750fb5a4155c40f50a5e38
Shunsuke Ohashi <shunsuke@indeed.com>
2017-07-13 18:48:20 +0900

    parser done

--- src/bms_parser.rs
+++ src/bms_parser.rs
@@ -5,12 +5,19 @@ use std::collections::HashMap;
 
 
 pub struct BmsScript {
-    headers: HashMap<String, String>
+    headers: HashMap<String, String>,
+    channels: HashMap<String, String>,
 }
 
 impl BmsScript {
     fn wav_file(&self, key: &str) -> &str {
         unimplemented!()
+    }
+
+    fn channel(&self, segment_id: u32, channel_id: u32) -> &str {
+        let key = format!("{0: <03}{1: <02}", segment_id, channel_id);
+        println!("{}", key);
+        self.channels.get(&key).unwrap()
     }
 }
 
@@ -38,6 +45,7 @@ struct BmsStringParser {
 impl BmsParser for BmsStringParser {
     fn parse(&self) -> BmsScript {
         let mut headers = HashMap::new();
+        let mut channels = HashMap::new();
         for line in self.script.split('\n') {
             let trimmed = line.trim();
             if trimmed.is_empty() {
@@ -48,7 +56,10 @@ impl BmsParser for BmsStringParser {
                         // command line or channel line
                         let channel_re = Regex::new(r"^#\d{5}:.*$").unwrap();
                         if channel_re.is_match(&trimmed) {
-                            println!("channel: {}", line);
+                            let key = &line[1..6];
+                            let value = &line[8..];
+                            channels.insert(key.to_string(), value.to_string());
+                            println!("channel: {} = {}", key, value);
                         } else {
                             let tokens: Vec<&str> = trimmed.split(' ').collect();
                             let key = &tokens.get(0).unwrap()[1..];
@@ -61,12 +72,13 @@ impl BmsParser for BmsStringParser {
                 };
             };
         };
-        BmsScript { headers: headers }
+        BmsScript { headers: headers, channels: channels }
     }
 }
 
 #[test]
 fn parser_test() {
     let bms = BmsFileParser { path: "example/conflict/_01_conflict.bme".to_string() }.parse();
-    println!("{}", bms.headers.get("GENRE").unwrap())
+    println!("{}", bms.headers.get("GENRE").unwrap());
+    println!("{}", bms.channel(1, 1))
 }
